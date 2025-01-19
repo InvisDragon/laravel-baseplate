c2282b22d52ce6092cd4acf8ddb8108e23c5bb8b
Joe Simpson <joe@invisibledragonltd.com>
2025-01-19 17:00:58 +0000

    Document title Vue component

---
+++ vue/DocumentTitle.vue
@@ -0,0 +1,22 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "DocumentTitle",
+    props: [ 'title' ],
+    computed: {
+        fullTitle() {
+            let t = this.title;
+            if(window['thisAppName']) {
+                t += " - " + window.thisAppName;
+            }
+            return t;
+        }
+    },
+    mounted() {
+        document.title = this.fullTitle;
+    }
+}
+</script>
