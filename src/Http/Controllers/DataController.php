b9561377b680c3e106af60a5d23c9da7ca571a76
Joe Simpson <joe@invisibledragonltd.com>
2025-10-04 13:20:25 +0100

    Data Controllers: Introduce ReadOnly version to ensure nothing can be updated in certain cases

--- src/Http/Controllers/DataController.php
+++ src/Http/Controllers/DataController.php
@@ -11,54 +11,31 @@ use InvisibleDragon\LaravelBaseplate\Data\DataDescriber;
 /**
  * Base class that interacts with Spatie's Data classes to provide a basis for CRUD operations
  */
-abstract class DataController
+abstract class DataController extends ReadOnlyDataController
 {
-    /**
-     * Return the class name of the Data subclass you wish to use
-     *
-     * @template T
-     *
-     * @return class-string<T>
-     */
-    abstract public function getDataClass();
 
     /**
-     * Return the data class used to view this specific object if you wish to override this
-     *
-     * @param $obj object Model object
-     * @return class-string<T>
+     * Return a single instance of the query (assumes id is primary key)
      */
-    public function getSingleDataClass($obj) {
-        return $this->getDataClass()::from($obj);
+    public function show(Request $request)
+    {
+        $obj = $this->getSingleObject($request);
+        Gate::authorize('show', $obj);
+        if ($obj) {
+            if($request->get('context') === 'edit') {
+                return ($this->getEditDataClass())::from($obj);
+            }
+            return $this->getSingleDataClass($obj);
+        } else {
+            abort(404);
+        }
     }
 
+    /**
+     * Returns the data class, if different, for editing data
+     */
     public function getEditDataClass() {
         return $this->getDataClass();
-    }
-
-    /**
-     * Return the class name of the Model subclass you wish to use
-     *
-     * @template T
-     *
-     * @return class-string<T>
-     */
-    abstract public function getModelClass();
-
-    /**
-     * Return the query of which you wish to base this controller around. This
-     * should include restrictions such as what the current request context/user is
-     * allowed to see
-     */
-    abstract public function getQuery(Request $request);
-
-    /**
-     * Return a list of the items
-     */
-    public function index(Request $request)
-    {
-        Gate::authorize('listAll', $this->getModelClass());
-        return $this->getDataClass()::collect($this->getQuery($request)->paginate());
     }
 
     /**
@@ -88,32 +65,6 @@ abstract class DataController
         $model->refresh();
 
         return $this->getSingleDataClass($model);
-    }
-
-    public function getSingleObject(Request $request)
-    {
-        $params = array_values( $request->route()->parameters() );
-        $id = array_pop( $params );
-        $query = $this->getQuery($request)->where('id', $id);
-        $obj = $query->first();
-        return $obj;
-    }
-
-    /**
-     * Return a single instance of the query (assumes id is primary key)
-     */
-    public function show(Request $request)
-    {
-        $obj = $this->getSingleObject($request);
-        Gate::authorize('show', $obj);
-        if ($obj) {
-            if($request->get('context') === 'edit') {
-                return ($this->getEditDataClass())::from($obj);
-            }
-            return $this->getSingleDataClass($obj);
-        } else {
-            abort(404);
-        }
     }
 
     /**
@@ -172,32 +123,8 @@ abstract class DataController
 
     public static function resourceRoutes(string $prefix)
     {
+        parent::resourceRoutes($prefix);
         Route::get($prefix.'/describe', [static::class, 'describe']);
-        Route::resource($prefix, static::class);
-        static::resourceRootTraits($prefix);
-    }
-
-    /**
-     * Code pattern from Laravel @
-     * https://github.com/laravel/framework/blob/7aabb896018f462bab291c50295ce613c8d840f3/src/Illuminate/Database/Eloquent/Model.php#L308
-     * @param string $prefix
-     * @return void
-     */
-    protected static function resourceRootTraits(string $prefix)
-    {
-        $class = static::class;
-        $booted = [];
-
-        foreach (class_uses_recursive($class) as $trait) {
-            $method = 'routes'.class_basename($trait);
-
-            if (method_exists($class, $method) && ! in_array($method, $booted)) {
-                forward_static_call([$class, $method], $prefix);
-
-                $booted[] = $method;
-            }
-
-        }
     }
 
 }
---
+++ src/Http/Controllers/ReadOnlyDataController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace InvisibleDragon\LaravelBaseplate\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Route;
+
+/**
+ * Base class that interacts with Spatie's Data classes to provide
+ * read-only access to data
+ */
+abstract class ReadOnlyDataController
+{
+    /**
+     * Return the class name of the Data subclass you wish to use
+     *
+     * @template T
+     *
+     * @return class-string<T>
+     */
+    abstract public function getDataClass();
+
+    /**
+     * Return the data class used to view this specific object if you wish to override this
+     *
+     * @param $obj object Model object
+     * @return class-string<T>
+     */
+    public function getSingleDataClass($obj) {
+        return $this->getDataClass()::from($obj);
+    }
+
+    /**
+     * Return the class name of the Model subclass you wish to use
+     *
+     * @template T
+     *
+     * @return class-string<T>
+     */
+    abstract public function getModelClass();
+
+    /**
+     * Return the query of which you wish to base this controller around. This
+     * should include restrictions such as what the current request context/user is
+     * allowed to see
+     */
+    abstract public function getQuery(Request $request);
+
+    /**
+     * Return a list of the items
+     */
+    public function index(Request $request)
+    {
+        Gate::authorize('listAll', $this->getModelClass());
+        return $this->getDataClass()::collect($this->getQuery($request)->paginate());
+    }
+
+    public function getSingleObject(Request $request)
+    {
+        $params = array_values( $request->route()->parameters() );
+        $id = array_pop( $params );
+        $query = $this->getQuery($request)->where('id', $id);
+        $obj = $query->first();
+        return $obj;
+    }
+
+    /**
+     * Return a single instance of the query (assumes id is primary key)
+     */
+    public function show(Request $request)
+    {
+        $obj = $this->getSingleObject($request);
+        Gate::authorize('show', $obj);
+        if ($obj) {
+            return $this->getSingleDataClass($obj);
+        } else {
+            abort(404);
+        }
+    }
+
+    public static function resourceRoutes(string $prefix)
+    {
+        Route::resource($prefix, static::class);
+        static::resourceRootTraits($prefix);
+    }
+
+    /**
+     * Code pattern from Laravel @
+     * https://github.com/laravel/framework/blob/7aabb896018f462bab291c50295ce613c8d840f3/src/Illuminate/Database/Eloquent/Model.php#L308
+     * @param string $prefix
+     * @return void
+     */
+    protected static function resourceRootTraits(string $prefix)
+    {
+        $class = static::class;
+        $booted = [];
+
+        foreach (class_uses_recursive($class) as $trait) {
+            $method = 'routes'.class_basename($trait);
+
+            if (method_exists($class, $method) && ! in_array($method, $booted)) {
+                forward_static_call([$class, $method], $prefix);
+
+                $booted[] = $method;
+            }
+
+        }
+    }
+
+}
